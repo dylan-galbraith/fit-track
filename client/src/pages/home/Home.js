979ebe4cf97511033b94d6447d7d50aee7224e12
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-03-24 19:54:00 -0400

    Add axios request on home page to dynamically load routines and favourites from database

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -1,33 +1,57 @@
 import { Component } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class Home extends Component {
+
+  state = {
+    routines: [],
+    favourites: []
+  }
+
+  componentDidMount = () => {
+    axios
+      .get('http://localhost:8070/routines')
+      .then( response => {
+        this.setState({
+          routines: response.data
+        })
+      })
+      .then(
+        axios
+          .get('http://localhost:8070/exercises')
+          .then( response => {
+            const favourites = response.data.filter(item => item.favourite)
+            this.setState({
+              favourites: favourites
+            })
+          })
+      )
+  }
+
   render() {
     return(
       <main className="main">
         <h1 className="main__heading">Welcome, Dylan!</h1>
         <article className="category">
-          <h3 className="category__heading">Routines</h3>
-          <div className="category__item">
-            <p className="category__name">Leg Day</p>
-          </div>
-          <div className="category__item">
-            <p className="category__name">Chest Day</p>
-          </div>
-          <div className="category__item">
-            <p className="category__name">Back Day</p>
-          </div>
+        <h3 className="category__heading">Routines</h3>
+          {this.state.routines.map(item => {
+            return (
+              <div className="category__item" key={item.id}>
+                <Link to='/routines/:routineId' className="category__name">{item.name}</Link>
+              </div>
+            )
+          })}
         </article>
         <article className="category">
           <h3 className="category__heading">Favourites</h3>
-          <div className="category__item">
-            <p className="category__name">Squat</p>
-          </div>
-          <div className="category__item">
-            <p className="category__name">Deadlift</p>
-          </div>
-          <div className="category__item">
-            <p className="category__name">Bench Press</p>
-          </div>
+          {this.state.favourites.map(item => {
+            return (
+              <div className="category__item" key={item.id}>
+                <Link to='/exercises/:exerciseId' className="category__name">{item.name}</Link>
+              </div>
+            )
+          })}
         </article>
       </main>
     )
