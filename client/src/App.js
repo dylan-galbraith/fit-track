c844f531488ede228e8dc20ea8c07b06d7d60409
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-03-24 19:29:06 -0400

    Create and style menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
 import './App.scss'
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Component } from 'react'
 
 import Footer from './components/Footer'
 import Home from './pages/home/Home'
 import Exercises from './pages/exercises/Exercises'
 import Routines from './pages/routines/Routines'
 
-function App() {
-  return (
-    <div className="app">
-      <BrowserRouter>
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/exercises' exact component={Exercises} />
-          <Route path='/routines' exact component={Routines} />
-        </Switch>
-      </BrowserRouter>
-      <Footer />
-    </div>
-  );
+class App extends Component {
+
+
+
+  render() {
+    return (
+      <div className="app">
+        <BrowserRouter>
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/exercises' exact component={Exercises} />
+            <Route path='/routines' exact component={Routines} />
+          </Switch>
+          <Footer/>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,12 +1,44 @@
-import menuIcon from '../assets/icons/icons8-menu.svg'
+import { Component } from 'react';
+import { NavLink, Link } from 'react-router-dom';
 
-function Footer() {
-  return(
-    <footer className="footer">
-      <h1 className="footer__logo">FitTrack</h1>
-      <img className="footer__icon" src={menuIcon} alt="menu icon" />
-    </footer>
-  )
+import menuIcon from '../assets/icons/icons8-menu.svg';
+import exitIcon from '../assets/icons/exit-icon.svg';
+
+class Footer extends Component {
+
+  state = {
+    menuOpen: false
+  }
+
+  menuHandler = (e) => {
+    this.setState({
+      menuOpen: !this.state.menuOpen
+    })
+  }
+
+  render() {
+    if (this.state.menuOpen) {
+      return (
+        <section className="menu">
+          <div className="menu__list">
+            <NavLink className="menu__item" to='/routines' onClick={this.menuHandler}>Routines</NavLink>
+            <NavLink className="menu__item" to='/favourites' onClick={this.menuHandler}>Favourites</NavLink>
+            <NavLink className="menu__item" to='/exercises' onClick={this.menuHandler}>Exercises</NavLink>
+          </div>
+          <footer className="footer">
+            <Link className="footer__logo" to='/'  onClick={this.menuHandler}>FitTrack</Link>
+            <img className="footer__icon" onClick={this.menuHandler} src={exitIcon} alt="menu icon" />
+          </footer>
+        </section>
+      )
+    }
+    return (
+      <footer className="footer">
+        <Link className="footer__logo" to='/'>FitTrack</Link>
+        <img className="footer__icon" onClick={this.menuHandler} src={menuIcon} alt="menu icon" />
+      </footer>
+    )
+  }
 }
 
 export default Footer; 
