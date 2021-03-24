0a877dc6f6b728dd6f0b3dc6a9d613ade752aabe
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-03-24 19:58:02 -0400

    Add axios request on Routines page to dynamically load routines from database

--- client/src/pages/routines/Routines.js
+++ client/src/pages/routines/Routines.js
@@ -1,25 +1,34 @@
+import axios from 'axios';
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Routines extends Component {
+
+  state = {
+    routines: []
+  }
+
+  componentDidMount = () => {
+    axios
+      .get('http://localhost:8070/routines')
+      .then(response => {
+        this.setState({
+          routines: response.data
+        })
+      })
+  }
+
   render() {
     return (
       <main className="main">
         <h1 className="main__heading">Routines</h1>
-          <div className="category__item">
-            <p className="category__name">Leg Day</p>
-          </div>
-          <div className="category__item">
-            <p className="category__name">Chest Day</p>
-          </div>
-          <div className="category__item">
-            <p className="category__name">Back Day</p>
-          </div>
-          <div className="category__item">
-            <p className="category__name">Shoulders/Arms</p>
-          </div>
-          <div className="category__item">
-            <p className="category__name">Abs</p>
-          </div>
+        {this.state.routines.map(item => {
+            return (
+              <div className="category__item" key={item.id}>
+                <Link to='/routines/:routineId' className="category__name">{item.name}</Link>
+              </div>
+            )
+          })}
       </main>
     )
   }
