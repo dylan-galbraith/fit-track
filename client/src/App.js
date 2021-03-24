2cc7f04b096cd05f7aaaafbad0ec3744731e4716
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-03-24 17:50:09 -0400

    Add fonts

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import './App.scss'
+import Footer from './components/Footer'
+
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+      <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return(
+    <footer className="footer">
+      <h1 className="footer__logo">FitTrack</h1>
+    </footer>
+  )
+}
+
+export default Footer; 
