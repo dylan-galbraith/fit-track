3226505d9638e960aeab72e1a9efc3af49d193cb
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-03-29 17:43:46 -0400

    Create delete functionality for SingleExercise

--- client/src/pages/exercises/Exercises.js
+++ client/src/pages/exercises/Exercises.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { Component } from 'react';
 import { Link } from 'react-router-dom';
 import filledStarIcon from '../../assets/icons/star.svg';
-import ellipIcon from '../../assets/icons/ellipsis-horizontal.svg';
+import addIcon from '../../assets/icons/add.svg';
 
 class Exercises extends Component {
 
@@ -20,17 +20,10 @@ class Exercises extends Component {
       })
   }
 
-  optionMenu = (e) => {
-    console.log(e);
-  }
-
   render() {
     return (
       <main className="exercises">
-        <h1 className="exercises__heading">Exercises <img onClick={this.optionMenu} className="exercises__icon" src={ellipIcon} /></h1>
-        <div className="exercises__options">
-          <Link to="exercises/add" >Add</Link>
-        </div>
+        <h1 className="exercises__heading">Exercises <Link to="/exercises/add"><img onClick={this.optionMenu} className="exercises__icon" src={addIcon} /></Link></h1>
         <div className="exercises__list">
           {this.state.exercises.map(item => {
             return (
--- client/src/pages/exercises/SingleExercise.js
+++ client/src/pages/exercises/SingleExercise.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import starIcon from '../../assets/icons/star-outline.svg';
 import filledStarIcon from '../../assets/icons/star.svg';
 import backIcon from '../../assets/icons/arrow-back.svg';
+import deleteIcon from '../../assets/icons/trash-outline.svg';
 import { Link } from 'react-router-dom';
 
 class SingleExercise extends Component {
@@ -26,6 +27,14 @@ class SingleExercise extends Component {
       })
   }
 
+  deleteHandler = () => {
+    axios
+      .delete(`http://localhost:8070/exercises/${this.props.match.params.exerciseId}`)
+      .then(response => {
+        console.log(response);
+      })
+  }
+
   componentDidMount = () => {
     axios
       .get(`http://localhost:8070/exercises/${this.props.match.params.exerciseId}`)
@@ -40,7 +49,7 @@ class SingleExercise extends Component {
     if (!this.state.exercise) return <p>Loading...</p>
     return (
       <main className="exercise">
-        <h1 className="exercise__heading"><Link to="/exercises"><img src={backIcon} className="exercise__icon" /></Link>{this.state.exercise.name} <img onClick={this.favouriteHandler} className="exercise__icon" src={this.state.exercise.favourite ? filledStarIcon : starIcon} /> </h1>
+        <h1 className="exercise__heading"><Link to="/exercises"><img src={backIcon} className="exercise__icon" /></Link>{this.state.exercise.name}<Link to="/exercises"><img src={deleteIcon} className="exercise__icon" onClick={this.deleteHandler} /></Link> <img onClick={this.favouriteHandler} className="exercise__icon" src={this.state.exercise.favourite ? filledStarIcon : starIcon} /> </h1>
         {this.state.exercise.record.map(item => {
           return (
             <div className="exercise__record" key={item.id}>
