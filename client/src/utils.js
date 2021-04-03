2352ee9fb1aa177af5491bf4f88795e9a6bfbd06
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-03 17:10:47 -0400

    deploying to heroku

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,1 +1,4 @@
-export const API_URL = 'http://localhost:8070';
+// export const API_URL = 'http://localhost:8070';
+export const API_URL = process.env.NODE_ENV === "production"
+? 'https://fit-track-dylan.herokuapp.com'
+: 'http://localhost:8070';
