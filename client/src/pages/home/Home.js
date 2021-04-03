bae3a22eab819eb17519e665d3e541f17db227fe
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-03 16:20:48 -0400

    Fix favourites bug, remove changes made to try and deploy

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -15,7 +15,7 @@ const Home = ({ user, routines, exercises }) => {
       <h3 className="home__category__heading">Favourites</h3>
       <article className="home__category">
         <Link to='/exercises' className={!exercises.find(item => item.favourite) ? "home__name home__name--top" : "hidden"}>Add a New Favourite!</Link>
-        {exercises.map(item => {
+        {exercises.filter(item => item.favourite).map(item => {
             return <Link key={item.id} to={`/exercises/${item.id}`} className={exercises.indexOf(item)===0 ? "home__name home__name--top" : "home__name"}>{item.name}</Link>
           }
         )}
--- client/src/utils.js
+++ client/src/utils.js
@@ -1,4 +1,1 @@
-// export const API_URL = 'http://localhost:8070';
-export const API_URL = process.env.NODE_ENV === "production"
-  ? 'https://fit-track-dylan.herokuapp.com'
-  : 'http://localhost:8070';
+export const API_URL = 'http://localhost:8070';
