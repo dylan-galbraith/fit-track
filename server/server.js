c7b9b1d5508b2bd23a916b792d93573438fb0d2a
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-17 17:02:49 -0400

    Clean up code in server

--- server/server.js
+++ server/server.js
@@ -3,10 +3,7 @@ const cors = require('cors');
 const exerciseRoutes = require('./routes/exercises');
 const routineRoutes = require('./routes/routines');
 const recordRoutes = require('./routes/records');
-const userRoutes = require('./routes/users');
-const mysql = require('mysql');
 require('dotenv').config();
-const passport = require('passport');
 
 const app = express();
 const PORT = process.env.PORT || 8070;
@@ -17,7 +14,6 @@ app.use(express.json());
 app.use('/exercises', exerciseRoutes);
 app.use('/routines', routineRoutes);
 app.use('/records', recordRoutes);
-app.use('/', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
