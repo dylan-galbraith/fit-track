943eaac8dac7d55ca431500c05b36b08a358d47c
Dylan Galbraith <dylangalbraith9@gmail.com>
2021-04-21 15:16:11 -0400

    Add responsive styling for Login, Singup, Home, and all Routine pages

--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -23,21 +23,27 @@ export default function Home({ getData }) {
   return(
     <main className="home">
       <h1 className="home__heading">Welcome, {currentUser.displayName}!</h1>
-      <h3 className="home__category__heading">Routines</h3>
-      <article className="home__category">
-        <Link to='/routines/add' className={info.routines.length===0 ? "home__name home__name--top" : "hidden"}>Add a New Routine!</Link>
-        {info.routines.map(item => {
-          return <Link key={item.id} to={`/routines/${item.id}`} className={info.routines.indexOf(item)===0 ? "home__name home__name--top" : "home__name"}>{item.name}</Link>
-        })}
-      </article>
-      <h3 className="home__category__heading">Favourites</h3>
-      <article className="home__category">
-        <Link to='/exercises' className={!info.exercises.find(item => item.favourite) ? "home__name home__name--top" : "hidden"}>Add a New Favourite!</Link>
-        {info.exercises.filter(item => item.favourite).map(item => {
-            return <Link key={item.id} to={`/exercises/${item.id}`} className={info.exercises.filter(item => item.favourite).indexOf(item)===0 ? "home__name home__name--top" : "home__name"}>{item.name}</Link>
-          }
-        )}
-      </article>
+      <div className="home__tablet">
+        <div className="home__tablet__box">
+          <h3 className="home__category__heading">Routines</h3>
+          <article className="home__category">
+            <Link to='/routines/add' className={info.routines.length===0 ? "home__name home__name--top" : "hidden"}>Add a New Routine!</Link>
+            {info.routines.map(item => {
+              return <Link key={item.id} to={`/routines/${item.id}`} className={info.routines.indexOf(item)===0 ? "home__name home__name--top" : "home__name"}>{item.name}</Link>
+            })}
+          </article>
+        </div>
+        <div className="home__tablet__box">
+          <h3 className="home__category__heading">Favourites</h3>
+          <article className="home__category">
+            <Link to='/exercises' className={!info.exercises.find(item => item.favourite) ? "home__name home__name--top" : "hidden"}>Add a New Favourite!</Link>
+            {info.exercises.filter(item => item.favourite).map(item => {
+                return <Link key={item.id} to={`/exercises/${item.id}`} className={info.exercises.filter(item => item.favourite).indexOf(item)===0 ? "home__name home__name--top" : "home__name"}>{item.name}</Link>
+              }
+            )}
+          </article>
+        </div>
+      </div>
     </main>
   )
 }
